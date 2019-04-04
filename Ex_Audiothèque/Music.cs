861b806e049bb31c43da045d3fadaff1d42d0532
Sylvain <Villsyl@divtec.ch>
2019-04-04 15:46:50 +0200

    Ajoutez des fichiers projet.

---
+++ Ex_Audiothèque/Music.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Audiothèque
+{
+    class Music
+    {
+        private string title;
+        private int duration;
+        private int rating;
+        public enum MusicRating
+        {
+            NO_STAR,
+            ONE_STAR,
+            TWO_STAR,
+            THREE_STAR
+
+              
+        }
+
+        List<Artiste> = new List<>();
+    }
+}
---
+++ Ex_Audiothèque/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Audiothèque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
